2e51fd258acf832bc9bd81be2d2fde90d5d4b6e3
Lupacescu Eduard <lupacescueduard@gmail.com>
2020-04-17 22:29:21 +0300

    Clone

--- aliases.zsh
+++ aliases.zsh
@@ -1,6 +1,7 @@
 # Shortcuts
 alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
 alias reloadcli="source $HOME/.zshrc"
+alias reload="source $HOME/.zshrc"
 alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
 alias ll="/usr/local/opt/coreutils/libexec/gnubin/ls -ahlF --color --group-directories-first"
 weather() { curl -4 wttr.in/${1:-antwerp} }
