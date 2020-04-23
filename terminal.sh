254ca0ebfbcfddbf1e7a6d1978330e96084b2fd5
Eduard Lupacescu <lupacescueduard@gmail.com>
2020-04-23 21:39:27 +0300

    wip

--- terminal.sh
+++ terminal.sh
@@ -11,3 +11,5 @@ bash -c "$(curl -sLo- https://git.io/JvvDs)"
 # Download and install https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Hack/Bold/complete/Hack%20Bold%20Nerd%20Font%20Complete.ttf
 # And select this font in the iTerm for directories icons
 
+# Fixing the issue with paste into terminal
+printf '\e[?2004l'
