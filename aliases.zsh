ad5530bd423cdb551b68f16e1a7695e22a6eab2a
Eduard Lupacescu <eduard.lupacescu@binarcode.com>
2022-04-12 14:22:10 +0300

    fix: alias paths

--- aliases.zsh
+++ aliases.zsh
@@ -6,6 +6,7 @@ alias ll="/usr/local/opt/coreutils/libexec/gnubin/ls -AhlFo --color --group-dire
 alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 alias aliases="vi $DOTFILES/aliases.zsh"
+alias paths="vi $DOTFILES/path.zsh"
 alias c="clear"
 
 # Directories
